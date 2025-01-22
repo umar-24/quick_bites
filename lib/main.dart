f077a6100f2206134ce1da74190ed676e6719415
Umar <macbook@MacBooks-MacBook-Pro.local>
2025-01-22 14:47:02 +0500

    adding login or register page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
-import 'package:quick_bites/features/onboarding/onboarding_screen.dart';
 import 'package:quick_bites/splash/splash_screen.dart';
 
 void main() {
